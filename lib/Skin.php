8f2b2b2ed089d5f1d665e9001fce01e4bfd6c033
Christophe VG <xtof@astroboy.local>
2011-03-24 12:28:37 +0100

    fixed wrong selection of current content added comments cleaned up file organization added visibility limiters

--- lib/Skin.php
+++ lib/Skin.php
@@ -1,6 +1,9 @@
 <?php
 
 abstract class Skin {
+  /**
+   * Factory Method to get a skin object based on its name.
+   */
   static function get( $name = 'default' ) {
     $skinFile = dirname(__FILE__) . "/../skins/$name.php";
     if( file_exists( $skinFile ) ) {
@@ -13,34 +16,46 @@ abstract class Skin {
     return null;
   }
   
+  /**
+   * Chaining method to set the user to which the content will be shown,
+   * using this skin.
+   */
   function to( $user = null ) {
     $this->user = $user;
     return $this;
   }
-  
+
+  /**
+   * Chaining method to set the content that will be shown, using this skin.
+   */
   function show( $content = "" ) {
     if( ! is_array( $this->contents ) ) { $this->contents = array(); }
     array_push( $this->contents, $content );
     return $this;
   }
 
+  /**
+   * In a string context, the skin is applied to the currently top content.
+   */
   function __toString() {
     $out = $this->applySkin();
     array_pop( $this->contents );
     return $out;
   }
   
-  function content() {
+  // below this point are internal functions
+
+  /**
+   * Returns the currently top-most content on the skinning stack.
+   */
+  protected function content() {
     return $this->contents[count($this->contents)-1];
   }
-  
-  function __get( $name ) {
-    if( method_exists( $this, $name ) ) {
-      return $this->$name();
-    }
-  }
-  
-  function subContent() {
+
+  /**
+   * Returns the assembled rendering of all sub-content items.
+   */
+  protected function subContent() {
     $subContent = "";
     foreach( $this->content->children as $child ) {
       $subContent .= $this->show( Content::get($child) );
@@ -48,10 +63,27 @@ abstract class Skin {
     return $subContent;
   }
 
+  /**
+   * A skin can reference methods through a getter interface.
+   */
+  function __get( $name ) {
+    if( method_exists( $this, $name ) ) {
+      return $this->$name();
+    }
+  }
+
+  /**
+   * Applies this skin to the currently topmost content. Based on the type
+   * a different method is chosen if it is available.
+   */
   private function applySkin() {
+    // if there is only 1 content object on the stack, it's a body, else
+    // we're dealing with sub-items.
     $type = count($this->contents) > 1 ? 'item' : 'body';
-    $contentType = str_replace( 'Content', '' , get_class($content) );
+
+    $contentType = str_replace( 'Content', '' , get_class($this->content) );
     $skinMethod = $contentType . 'As' . ucfirst( $type );
+
     if( method_exists( $this, $skinMethod ) ) {
       return $this->$skinMethod();
     } elseif( method_exists( $this, $type ) ) {
@@ -60,5 +92,4 @@ abstract class Skin {
       return (string)$this->content;
     }
   }
-
 }
